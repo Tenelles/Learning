9b088e4c94529ba670dbecaf391675a48673674c
Tenelles <tenelles@yandex.ru>
2021-06-07 14:17:00 +0300

    added basic interfaces

--- Enemy.cs
+++ Enemy.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-class Enemy
+class Enemy : Mob
 {
+    public Enemy(int hp) : base(hp)
+    {
+    }
+
+    public Enemy(int hp, Vector position) : base(hp, position)
+    {
+    }
+
 
 }
--- Program.cs
+++ Program.cs
@@ -4,11 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        BasicItem[] itemList = { new Weapon(), new Food() };
+
+        Mob slime = new Enemy(100);
+
+        foreach (var item in itemList)
+            Use(item, slime);
+
+        MobPositionRandomizer(new Mob[] { slime, new Enemy(10) });
+
+
+        IKillable target = slime;
+        target.Death();
     }
 
-    static void foo()
+    static void Use(BasicItem item, Mob target)
     {
+        item.Action(target);
+    }
 
+    static void MobPositionRandomizer(IMovable[] mobs)
+    {
+        foreach (var mob in mobs)
+        {
+            mob.MoveTo(new Vector(1, 2));
+            // TODO: добавить рандомайзер
+        }
     }
 }
