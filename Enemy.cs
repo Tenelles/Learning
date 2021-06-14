9572c9ef2b75e0a551daead7c6ccb733099621a3
NiaRelia <kalinina-kristina@inbox.ru>
2021-06-14 17:59:20 +0300

    hw, classes from June 4

--- Enemy.cs
+++ Enemy.cs
@@ -1,8 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-class Enemy
+class Enemy : Mob
 {
+    // Переменные
+    public readonly float Speed; // Скорость только для чтения. Изменить можно лишь в конструкторе данного класса или задать значение при инициализации
+
+    // Конструктор
+    public Enemy(float hp, float speed) : base(hp) => Speed = speed; // добавили скорость к конструктору mob'а
+    public Enemy(float hp, Vector position, float speed) : base(hp, position) => Speed = speed; // перегрузка конструктора, добавили скорость к конструктору mob'а
+
+    // Методы
+
+    // Приблизиться к точке (Перемещается на небольшое расстояние по направлению к указанной точке)
+    public void Attack(Mob mob, float damage) // Атаковать указанного игрока указанным уроном в случае, если можно атаковать
+    {
+        if (CanAttack(mob)) // Если можно атаковать указанного моба
+        {
+            mob.TakeDamage(damage); // Указанный моб получает урон
+        }
+    }
+    public bool CanAttack(Mob mob) => Position == mob.Position; // Если позиция enemy равна позиции mob, то true, иначе false
 
 }
--- Vector.cs
+++ Vector.cs
@@ -4,21 +4,31 @@ using System.Text;
 
 class Vector
 {
-    public float X { get; private set; }
-    public float Y { get; private set; }
+    public float X { get; private set; } // read everywhere, change in class Vector
+    public float Y { get; private set; } // read everywhere, change in class Vector
 
-    public Vector(float x = 0, float y = 0)
+    public Vector(float x = 0, float y = 0) // constructor, (?) x = 0, в случае если ничего не передавали.
     {
         X = x;
         Y = y;
     }
 
-    public static Vector Zero => new Vector(0, 0);
+    public Vector Normalized => Length != 0 ? new Vector(X / Length, Y / Length) : Zero;
 
-    public static Vector Plus(Vector first, Vector second) => new Vector(first.X + second.X, first.Y + second.Y);
-    public static Vector Minus(Vector first, Vector second) => new Vector(first.X - second.X, first.Y - second.Y);
-    public static Vector Multiply(Vector point, float number) => new Vector(point.X * number, point.Y * number);
-    public static Vector Divide(Vector point, float number) => new Vector(point.X / number, point.Y / number);
-    public static bool AreEqual(Vector first, Vector second) => first.X == second.X && first.Y == second.Y;
+    public float Length => MathF.Sqrt(MathF.Pow(X, 2) + MathF.Pow(Y, 2));
+
+    public static Vector Zero => new Vector(0, 0); // static method Zero, returns the zero point
+
+    public static Vector operator +(Vector first, Vector second) => new Vector(first.X + second.X, first.Y + second.Y);
+    public static Vector operator -(Vector first, Vector second) => new Vector(first.X - second.X, first.Y - second.Y);
+    public static Vector operator *(Vector vector, float value) => new Vector(vector.X * value, vector.Y * value);
+    public static Vector operator *(float value, Vector vector) => vector * value;
+    public static Vector operator /(Vector vector, float value) => new Vector(vector.X / value, vector.Y / value);
+    public static bool operator ==(Vector first, Vector second) => first.X == second.X && first.Y == second.Y;
+    public static bool operator !=(Vector first, Vector second) => first.X != second.X || first.Y != second.Y;
+
+    public override bool Equals(object obj) => obj.GetType() == GetType() && (Vector)obj == this;
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public override string ToString() => "(" + X.ToString() + "; " + Y.ToString() + ")";
 
 }
